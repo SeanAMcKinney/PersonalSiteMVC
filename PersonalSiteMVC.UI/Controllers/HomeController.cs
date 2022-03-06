2729666d835305db3ed0e2efa378576f5dd09614
Sean McKinney <seanamckinney8@gmail.com>
2022-03-06 17:14:07 -0600

    Completed complete MVC conversion, got contact AJAX set up.  Still no luck with SmarterASP mail connect.

--- PersonalSiteMVC.UI/Controllers/HomeController.cs
+++ PersonalSiteMVC.UI/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PersonalSiteMVC.UI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,18 +16,34 @@ namespace PersonalSiteMVC.UI.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
         {
-            ViewBag.Message = "Your application description page.";
+            string body = $"You have received an email form <strong>{cvm.Name}</strong>. the email address given was <strong>{cvm.Email}</strong>.<br/><strong>The following message was sent:</strong> {cvm.Message}";
 
-            return View();
+            MailMessage mm = new MailMessage();
+            mm.From = new MailAddress("administrator@mckinneywebdev.com");
+            mm.To.Add(new MailAddress("seanamckinney8@gmail.com"));
+            mm.Subject = cvm.Subject;
+            mm.Body = body;
+
+            mm.IsBodyHtml = true;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient smtp = new SmtpClient("mail.mckinneywebdev.com");
+            NetworkCredential cred = new NetworkCredential("administrator@mckinneywebdev.com", "eight8ate*");
+
+            smtp.Credentials = cred;
+            smtp.Send(mm);
+            return Json(cvm);
         }
 
-        public ActionResult Contact()
+        public PartialViewResult ContactConfirmation(string name, string email)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            ViewBag.Name = name;
+            ViewBag.Email = email;
+            return PartialView("ContactConfirmation");
         }
-    }
-}
+
+    }//end class
+}//end namespace
